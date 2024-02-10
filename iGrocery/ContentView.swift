f297f7c785e7560cc8d0d64c3307db74a19020af
Yan Spatt <minerrbr@gmail.com>
2024-02-09 21:23:59 -0300

    Initial Commit

---
+++ iGrocery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iGrocery
+//
+//  Created by Yan Spatt on 09/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iGrocery/iGroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  iGroceryApp.swift
+//  iGrocery
+//
+//  Created by Yan Spatt on 09/02/24.
+//
+
+import SwiftUI
+
+@main
+struct iGroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
